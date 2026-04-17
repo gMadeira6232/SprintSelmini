95df9f394eae9e3f27f84edcb51779c6bf7970fd
Guilherme <guilhermemadeira@outlook.com>
2026-04-17 00:27:07 -0300

    Teste Commit

---
+++ trajeto.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class trajeto {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String direcao;
+        int distancia;
+
+        System.out.println("Informe a Direção de movimento do Robô");
+
+    }
+}
